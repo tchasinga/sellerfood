67400aa5371bd9a9638e2ee377242cd4b31f2378
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-08 13:48:27 +0300

    Refactor user routes for consistent formatting and improved readability

--- backend/routes/user.models.js
+++ backend/routes/user.models.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import { createNewUser, getAlldatafromtable, getDatabyID } from '../controllers/user.controllers.js';
+import express from "express";
+import {
+  createNewUser,
+  getAlldatafromtable,
+  getDatabyID,
+} from "../controllers/user.controllers.js";
 
 // initate express router
 const router = express.Router();
 
-router.get('/getalluser', getAlldatafromtable);
-router.get('/getuserbyid/:id', getDatabyID);
-router.post('/createuser', createNewUser);
-
-
+router.get("/getalluser", getAlldatafromtable);
+router.get("/getuserbyid/:id", getDatabyID);
+router.post("/createuser", createNewUser);
 
 export default router;
